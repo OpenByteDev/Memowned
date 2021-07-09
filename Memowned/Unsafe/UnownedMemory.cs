584c864bbf69ab7502a82f10809f979b96f3c861
OpenByte <development.openbyte@gmail.com>
2021-07-09 16:19:53 +0200

    Fix switched file contents

--- Memowned/Unsafe/UnownedMemory.cs
+++ Memowned/Unsafe/UnownedMemory.cs
@@ -1,31 +1,35 @@
 ﻿using System;
+using System.Buffers;
 using System.ComponentModel;
 using Memowned.Interfaces;
 
 namespace Memowned {
     /// <summary>
-    /// An <see cref="IReadOnlyMemoryOwner{T}"/> implementation that wraps a <see cref="ReadOnlyMemory{T}"/> instance.
+    /// An <see cref="IMemoryOwner{T}"/> implementation that wraps a <see cref="Memory{T}"/> instance.
     /// </summary>
     /// <typeparam name="T">The type of items stored.</typeparam>
-    public readonly struct ReadOnlyUnownedMemory<T> : IReadOnlyMemoryOwner<T>, IEquatable<ReadOnlyUnownedMemory<T>> {
+    public readonly struct UnownedMemory<T> : IMemoryOwner<T>, IReadOnlyMemoryOwner<T>, IEquatable<UnownedMemory<T>> {
         /// <inheritdoc/>
-        public ReadOnlyMemory<T> Memory { get; }
+        public Memory<T> Memory { get; }
 
         /// <summary>
-        /// Gets a <see cref="ReadOnlySpan{T}"/> wrapping the rented memory.
+        /// Gets a <see cref="Span{T}"/> wrapping the rented memory.
         /// </summary>
-        public ReadOnlySpan<T> Span => Memory.Span;
+        public Span<T> Span => Memory.Span;
 
         /// <summary>
         /// The number of items in the current instance.
         /// </summary>
         public int Length => Memory.Length;
 
+        /// <inheritdoc/>
+        ReadOnlyMemory<T> IReadOnlyMemoryOwner<T>.Memory => Memory;
+
         /// <summary>
-        /// Constructs a new <see cref="ReadOnlyUnownedMemory{T}"/> instance wrapping the given <see cref="ReadOnlyMemory{T}"/>.
+        /// Constructs a new <see cref="UnownedMemory{T}"/> instance wrapping the given <see cref="Memory{T}"/>.
         /// </summary>
-        /// <param name="memory">The <see cref="ReadOnlyMemory{T}"/> to wrap.</param>
-        public ReadOnlyUnownedMemory(ReadOnlyMemory<T> memory) =>
+        /// <param name="memory">The <see cref="Memory{T}"/> to wrap.</param>
+        public UnownedMemory(Memory<T> memory) =>
             Memory = memory;
 
         /// <inheritdoc/>
@@ -33,17 +37,17 @@ namespace Memowned {
 
         /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public override bool Equals(object? other) => other is ReadOnlyUnownedMemory<T> memory && Equals(memory);
+        public override bool Equals(object? other) => other is UnownedMemory<T> memory && Equals(memory);
 
         /// <inheritdoc/>
-        public bool Equals(ReadOnlyUnownedMemory<T> other) => Memory.Equals(other.Memory);
+        public bool Equals(UnownedMemory<T> other) => Memory.Equals(other.Memory);
 
         /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => Memory.GetHashCode();
 
         /// <summary>
-        /// For <see cref="ReadOnlyMemory{char}"/>, returns a new instance of string that represents the characters pointed to by the memory.
+        /// For <see cref="Memory{char}"/>, returns a new instance of string that represents the characters pointed to by the memory.
         /// Otherwise, returns a <see cref="string"/> with the name of the type and the number of elements.
         /// </summary>
         public override string ToString() {
@@ -52,21 +56,21 @@ namespace Memowned {
                 return Memory.ToString();
 
             // Same representation used in Memory<T>
-            return $"ReadOnlyUnownedMemory<{typeof(T)}>[{Length.ToString()}]";
+            return $"UnownedMemory<{typeof(T)}>[{Length.ToString()}]";
         }
 
         /// <summary>
-        /// Returns an empty <see cref="ReadOnlyUnownedMemory{T}"/> instance.
+        /// Returns an empty <see cref="UnownedMemory{T}"/> instance.
         /// </summary>
-        public static ReadOnlyUnownedMemory<T> Empty => default;
+        public static UnownedMemory<T> Empty => default;
 
-        public static implicit operator ReadOnlyMemory<T>(ReadOnlyUnownedMemory<T> memory) => memory.Memory;
-        public static implicit operator ReadOnlySpan<T>(ReadOnlyUnownedMemory<T> memory) => memory.Span;
-        public static implicit operator ReadOnlyOwnedMemory<T, ReadOnlyUnownedMemory<T>>(ReadOnlyUnownedMemory<T> memory) => ReadOnlyOwnedMemory<T>.Owned(memory);
+        public static implicit operator Memory<T>(UnownedMemory<T> memory) => memory.Memory;
+        public static implicit operator Span<T>(UnownedMemory<T> memory) => memory.Span;
+        public static implicit operator OwnedMemory<T, UnownedMemory<T>>(UnownedMemory<T> memory) => OwnedMemory<T>.Owned(memory);
 
-        public static explicit operator ReadOnlyOwnedSpan<T, ReadOnlyUnownedMemory<T>>(ReadOnlyUnownedMemory<T> memory) => ReadOnlyOwnedSpan<T>.Owned(memory.Span, memory);
+        public static explicit operator OwnedSpan<T, UnownedMemory<T>>(UnownedMemory<T> memory) => OwnedSpan<T>.Owned(memory.Span, memory);
 
-        public static bool operator ==(ReadOnlyUnownedMemory<T> left, ReadOnlyUnownedMemory<T> right) => left.Equals(right);
-        public static bool operator !=(ReadOnlyUnownedMemory<T> left, ReadOnlyUnownedMemory<T> right) => !(left == right);
+        public static bool operator ==(UnownedMemory<T> left, UnownedMemory<T> right) => left.Equals(right);
+        public static bool operator !=(UnownedMemory<T> left, UnownedMemory<T> right) => !(left == right);
     }
 }
